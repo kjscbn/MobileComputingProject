edfe7cf215fc35ea94b69c60eab9bfacc9182d98
kjscbn <kjscbn@umsystem.edu>
2021-04-14 13:17:38 -0500

    Worked on user checking

--- JavaServer/src/main/UserChecking.java
+++ JavaServer/src/main/UserChecking.java
@@ -1,5 +1,9 @@
 package main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class UserChecking {
 	public boolean doesUserExist(String username) {
 		return FileUtils.checkIfFileExists(username);
@@ -13,5 +17,39 @@ public class UserChecking {
 			return false;
 		}
 		return true;
+	}
+	
+	public static Human loadHumanFromFile(String filename) {
+		Human human2 = new Human(null, null, 0);
+		int count = 0;
+		
+		try {
+			File myObj = new File(filename);
+			Scanner myReader = new Scanner(myObj);
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				switch(count) {
+				case 0:
+					human2.setUsername(data);
+					break;
+				case 1:
+					human2.setCurrentIP(data);
+					break;
+				case 2:
+					human2.setScore(0);
+					break;
+				default:
+					break;
+				}
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return human2;
+	}
+	
+	public int calculateScore(int currentScore, int timeConnectedToCurrentIP) {
+		return currentScore * timeConnectedToCurrentIP;
 	}
 }
